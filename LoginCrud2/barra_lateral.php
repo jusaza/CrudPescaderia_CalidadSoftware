75443f9ee150c6161ea985c24e0033afc959fc74
Aprendiz <sena@sena.edu.co>
2025-12-04 07:17:14 -0500

    Tabulaciones BarraLtaeral.php

--- LoginCrud2/barra_lateral.php
+++ LoginCrud2/barra_lateral.php
@@ -2,23 +2,17 @@
 session_start();
 include_once ('conexion.php');
 
-if(isset($_SESSION['usuarioingresando']))
-{
+if (isset($_SESSION['usuarioingresando'])) {
     $usuarioingresado = $_SESSION['usuarioingresando'];
-
     $stmt = $conn->prepare("SELECT * FROM usuarios WHERE correo = ?");
     $stmt->bind_param("s", $usuarioingresado);
     $stmt->execute();
     $resultado = $stmt->get_result();
     $mostrar = $resultado->fetch_assoc();
+} else {
+    header('location: index.php');
 }
-else
-{
-	header('location: index.php');
-}
-
 ?>
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -27,14 +21,12 @@ else
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
 <div class="BarraLateral">
-
 <ul>
-<li><a href="dashboard.php" >• Dashboard</a></li>
-<li><a href="productos_tabla.php" >• Productos</a></li>
-<li><a href="categoria_tabla.php" >• Categoría</a></li>
-<li><a href="cerrar_sesion.php" >• Cerrar sesión</a></li>
+<li><a href="dashboard.php">• Dashboard</a></li>
+<li><a href="productos_tabla.php">• Productos</a></li>
+<li><a href="categoria_tabla.php">• Categoría</a></li>
+<li><a href="cerrar_sesion.php">• Cerrar sesión</a></li>
 </ul>
 <hr>
 </div>
